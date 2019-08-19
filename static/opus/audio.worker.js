82f959c4dc3bdbe7971980221f079ef55711ffd9
Sarisia <deploy@travis-ci.org>
2019-08-19 14:51:42 +0000

    Deploy gaiji.pro to github.com/aria-music/aria-music.github.io.git:master

--- static/opus/audio.worker.js
+++ static/opus/audio.worker.js
@@ -1,11 +1,11 @@
 import opusModule from './opus.js'
 
 
-const FRAME_SIZE = 960
-const FLUSH_SIZE = FRAME_SIZE * 25
+const FRAME_SIZE = 960 // 0.02s (20ms)
+const FLUSH_SIZE = FRAME_SIZE * 5
 const FLUSH_PACKET_SIZE = FLUSH_SIZE * 2
 
-let buf = new Float32Array(48000)
+let buf = new Float32Array(FLUSH_PACKET_SIZE)
 let count = 0
 
 let rawU8
@@ -16,17 +16,18 @@ const opus = opusModule({
         opus.ccall('init', 'number')
         const encodePtr = opus.ccall('encode_buf', 'number')
         const decodePtr = opus.ccall('decode_buf', 'number')
-        console.log(decodePtr)
+        // console.log(decodePtr)
         rawU8 = new Uint8Array(opus.HEAPU8.buffer, encodePtr)
         decodedF32 = new Float32Array(opus.HEAPF32.buffer, decodePtr)
         ready = true
+        console.log('[AudioWorker] Opus is ready!')
     }
 })
-console.log(opus)
+// console.log(opus)
 
-function flushBuffer() {
-    postMessage(buf.buffer, [buf.buffer])
-    buf = new Float32Array(48000)
+function flushBuffer(dispose=false) {
+    postMessage({'buf': buf.buffer, 'len': dispose ? 0 : count}, [buf.buffer])
+    buf = new Float32Array(FLUSH_PACKET_SIZE)
     count = 0
 }
 
@@ -34,8 +35,8 @@ function queueAudio(packet) {
     rawU8.set(new Uint8Array(packet))
     const frames = opus.ccall('decode', 'number', ['number'], [packet.byteLength])
     // console.log(frames)
-    // postMessage(decodedF32.buffer, [decodedF32.buffer])
-    for (let x = 0; x < FRAME_SIZE*2; ++x) {
+
+    for (let x = 0; x < frames*2; ++x) {
         buf[count++] = decodedF32[x]
     }
 
@@ -47,14 +48,42 @@ function queueAudio(packet) {
 let ws
 
 onmessage = (event) => {
+    // expected json as event.data
+    const packet = event.data
+    console.log(`[AudioWorker] received: ${packet.op}`)
+
+    switch (packet.op) {
+        case 'connect':
+            doConnect(packet)
+            break
+        case 'flush':
+            doFlush()
+            break
+        default:
+            break
+    }
+}
+
+function doConnect(payload) {
+    if (!payload.key)
+        return
+
+    try {
+        ws.close()
+    } catch (e) {}
+
     ws = new WebSocket("wss://sarisia.cc/stream/")
     ws.binaryType ="arraybuffer"
     ws.onopen = () => {
-        ws.send(event.data)
-        console.log("connected")
+        ws.send(payload.key)
+        console.log("[AudioWorker] Connected")
     }
     ws.onmessage = (packet) => {
         if (ready)
             queueAudio(packet.data)
     }
+}
+
+function doFlush() {
+    flushBuffer(true)
 }
